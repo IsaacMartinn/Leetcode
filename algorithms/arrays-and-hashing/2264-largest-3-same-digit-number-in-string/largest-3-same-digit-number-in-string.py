4b23aaf8807ebe04ee0925c2e952281ecee8140e
Isaac Martin <isaacisamartin@gmail.com>
2026-01-13 12:46:05 -0500

    Problem 2264

---
+++ algorithms/arrays-and-hashing/2264-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestGoodInteger(self, num):
+        max_total = ""
+        current_ord = highest_ord = -1
+
+        for i in range(len(num)-2):
+            if num[i] == num[i+1] == num[i+2]:
+                current_ord = ord(num[i])
+                highest_ord = max(current_ord,highest_ord)
+        
+        if highest_ord == -1:
+            return ""
+
+        max_total = chr(highest_ord)+chr(highest_ord)+chr(highest_ord)
+        return max_total
+        
