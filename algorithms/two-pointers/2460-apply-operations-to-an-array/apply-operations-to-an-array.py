2d515006e748a51934e0b307f9b0441d859ff37d
Isaac Martin <isaacisamartin@gmail.com>
2026-01-26 20:13:28 -0500

    Problem 2460

---
+++ algorithms/two-pointers/2460-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def applyOperations(self, nums):
+        
+        for i in range(len(nums)):
+            if i + 1 < len(nums): 
+                if nums[i] != nums[i + 1]:
+                    continue
+                else:
+                    nums[i] = nums[i] * 2
+                    nums[i + 1] = nums[i + 1] * 0
+        
+        l = 0
+        for r in range(len(nums)):
+            if nums[r]:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
