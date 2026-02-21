29bb86352539f4c117652e9a16e1cbf7fae484f0
Isaac Martin <isaacisamartin@gmail.com>
2026-02-21 12:10:03 -0500

    Problem 2390

---
+++ algorithms/stacks/2390-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        if not s:
+            return ""
+            
+        stack = []
+
+        for i in range(len(s)):
+            if s[i] == "*":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(s[i])
+        return "".join(stack)
+        
