891865aa339eecc0242bc2071919c01cc3bf44cb
Isaac Martin <isaacisamartin@gmail.com>
2026-03-12 14:16:13 -0400

    Problem 1209

---
+++ algorithms/stacks/1209-remove-all-adjacent-duplicates-in-string-II/remove-all-adjacent-duplicates-in-string-II.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = [] #char, count
+
+        for char in s:
+            if stack and stack[-1][0] == char:
+                stack[-1][1] += 1
+            else:
+                stack.append([char,1])
+            if stack[-1][1] == k:
+                stack.pop()
+
+        res = ""
+        for char, count in stack:
+            res += (char * count)
+        return res 
